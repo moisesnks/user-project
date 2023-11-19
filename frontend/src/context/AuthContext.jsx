eb8393d0164a674fd40611655f34f27660182768
moisesnks <mleiva@utem.cl>
2023-11-19 03:07:25 -0300

    Add Firebase dependencies and update AuthContext.jsx

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,27 +1,43 @@
 // src/context/AuthContext.jsx
 import React, { createContext, useContext, useState } from 'react';
+import { auth } from '../../firebase';
+import { signInWithEmailAndPassword, signOut, createUserWithEmailAndPassword } from 'firebase/auth';
 
 const AuthContext = createContext(null);
 
 export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
 
-    const login = (email, password) => {
-        // Aquí deberías añadir la lógica para verificar las credenciales.
-        // Por ejemplo, podrías llamar a una API y, en base a la respuesta, establecer el usuario.
-        const userData = { email }; // Simulación de datos de usuario
-        setUser(userData);
+    // Función de inicio de sesión que llama a 'signInWithEmailAndPassword' de Firebase Auth
+    const login = async (email, password) => {
+        try {
+            const userCredential = await signInWithEmailAndPassword(auth, email, password);
+            const userData = userCredential.user;
+            setUser(userData);
+        } catch (error) {
+            console.error('Error al iniciar sesión:', error.message);
+        }
     };
 
-    const logout = () => {
-        setUser(null);
+    // Función de cierre de sesión
+    const logout = async () => {
+        try {
+            await signOut(auth); // Utiliza la función signOut para cerrar la sesión
+            setUser(null);
+        } catch (error) {
+            console.error('Error al cerrar sesión:', error.message);
+        }
     };
 
-    const register = (email, password) => {
-        // Aquí deberías añadir la lógica para registrar un nuevo usuario.
-        // Por ejemplo, podrías llamar a una API para crear una cuenta y luego iniciar sesión automáticamente.
-        const userData = { email }; // Simulación de datos de usuario registrado
-        setUser(userData);
+    // Función de registro
+    const register = async (email, password) => {
+        try {
+            const userCredential = await createUserWithEmailAndPassword(auth, email, password); // Utiliza la función createUserWithEmailAndPassword para registrar nuevos usuarios
+            const userData = userCredential.user;
+            setUser(userData);
+        } catch (error) {
+            console.error('Error al registrar usuario:', error.message);
+        }
     };
 
     return (
